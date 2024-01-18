403e5bce4a1389b7956616115a5a0a8e4e8efc67
Code32123 <jimmy.l.smythe@gmail.com>
2024-01-17 20:38:54 -0800

    Volume

--- micVolumeUp.sh
+++ micVolumeUp.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-[[ "$(pactl get-source-volume 50)" =~ ([0-9]{1,3})% ]]
+[[ "$(pactl get-source-volume alsa_input.usb-Blue_Microphones_Yeti_Nano_2043SG001AM8_888-000154041006-00.analog-stereo)" =~ ([0-9]{1,3})% ]]
 if [ $? ]
 	then if (( ${BASH_REMATCH[1]} < 100))
-		then pactl set-source-volume 50 +5%
+		then pactl set-source-volume alsa_input.usb-Blue_Microphones_Yeti_Nano_2043SG001AM8_888-000154041006-00.analog-stereo +5%
 	fi
 fi
+
--- volume.py
+++ volume.py
@@ -5,27 +5,35 @@ HEADPHONES = [
 r"b'alsa_output.usb-Blue_Microphones_Yeti_Nano_2043SG001AM8_888-000154041006-00.analog-stereo\n'",
 ]
 
-def run_pactl_command(cmd):
-	cmd = subprocess.run(cmd, stdout = subprocess.PIPE, shell=True)
+def run_command(cmd):
+	cmd = subprocess.run(cmd, stdout = subprocess.PIPE, stderr = subprocess.DEVNULL, shell=True)
 	return str(cmd.stdout)
 
 def printVolumes():
+	if "true" == run_command("pamixer --get-mute")[2:-3]:
+		print("%{F#707880}muted%{F-}")
+		return
 	try:
-		micVol = run_pactl_command("pactl get-source-volume 50")
+		micVol = run_command("pactl get-source-volume alsa_input.usb-Blue_Microphones_Yeti_Nano_2043SG001AM8_888-000154041006-00.analog-stereo")
 		micVolList = micVol.split(" / ")
-		micVolLeft = micVolList[1].strip()
+		micVol = micVolList[1].strip()
 	except:
-		micVolLeft = None
+		micVol = None
 
-	spkName = run_pactl_command("pactl get-default-sink")
+	spkName = run_command("pactl get-default-sink")
 	spkPrefix = "" if spkName in HEADPHONES else ""
 
-	spkVol = run_pactl_command("pactl get-sink-volume $(pactl get-default-sink)")
-	spkVolList = spkVol.split(" / ")
-	spkVolLeft = spkVolList[1].strip()
+	# spkVol = run_command("pactl get-sink-volume $(pactl get-default-sink)")
+	# spkVolList = spkVol.split(" / ")
+	# spkVol = spkVolList[1].strip()
+	spkVol = run_command("pamixer --get-volume-human")[2:-3]
+	if spkVol == "muted":
+		print("%{F#707880}muted%{F-}")
+		return
 
 
-	print(spkPrefix, spkVolLeft, (" " + micVolLeft) if micVolLeft != None else "", flush=True)
+
+	print(f"{spkPrefix} {spkVol}" + (f"  {micVol}" if micVol != None else ""), flush=True)
 
 printVolumes()
 with pulsectl.Pulse('event-printer') as pulse:
@@ -36,6 +44,7 @@ with pulsectl.Pulse('event-printer') as pulse:
 		printVolumes()
 		### Raise PulseLoopStop for event_listen() to return before timeout (if any)
 		# raise pulsectl.PulseLoopStop
-	pulse.event_mask_set(pulsectl.PulseEventMaskEnum.sink, pulsectl.PulseEventMaskEnum.source)
+	# pulse.event_mask_set(pulsectl.PulseEventMaskEnum.sink, pulsectl.PulseEventMaskEnum.source)
+	pulse.event_mask_set(pulsectl.PulseEventMaskEnum.all)
 	pulse.event_callback_set(print_events)
-	pulse.event_listen(timeout=30)
+	pulse.event_listen(timeout=10)
