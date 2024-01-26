ee2d58b9913ee14e1eb151b56beaa759840a4cbb
Code32123 <jimmy.l.smythe@gmail.com>
2024-01-25 23:07:43 -0800

    Update wifidmenu.py

--- wifidmenu.py
+++ wifidmenu.py
@@ -6,7 +6,10 @@ def run_command(cmd):
 	cmd = subprocess.run(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
 	return str(cmd.stdout)[2:-1]
 
-networks = run_command("nmcli --get-values=SSID --colors=no d wifi list").split("\\n")
+NotificationId = run_command("notify-send --urgency=low -i 0 \"Goin' on a wifi hunt...\" -p")[:-2]
+print("NotifID", NotificationId)
+
+networks = run_command("nmcli --get-values=SSID --colors=no d wifi list --rescan yes").split("\\n")
 
 SSIDs = []
 
@@ -19,10 +22,13 @@ disconnect = "disconnect"
 chosenSSID = run_command(f"echo \"{disconnect}\\n" + "\\n".join(SSIDs) + "\" | dmenu -p \"SSID:\"").replace("\\n", "")
 
 if chosenSSID == "":
+	run_command(f"notify-send --urgency=low -i 0 \"Canceled.\" -r {NotificationId}")
 	exit("No network selected")
 
 if chosenSSID == disconnect:
+	run_command(f"notify-send --urgency=low -i 0 \"Disconnecting...\" -r {NotificationId}")
 	wifiDevice = [x.split(":")[0] for x in run_command("nmcli --get-values=DEVICE,TYPE --colors=no d").split("\\n") if x.split(":")[-1] == "wifi"][0]
 	run_command(f"nmcli d disconnect {wifiDevice}")
 else:
+	run_command(f"notify-send --urgency=low -i 0 \"Connecting to {chosenSSID}...\" -r {NotificationId}")
 	run_command(f"nmcli d wifi connect \"{chosenSSID}\"")
